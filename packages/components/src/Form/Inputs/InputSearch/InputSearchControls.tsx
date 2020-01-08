34b1371da1b49d5356c3a311c4b1e92a86693bcb
Meredith Dodge <meredith.dodge@looker.com>
2020-01-08 13:09:33 -0800

    Move InputChip on top of InputSearch

--- packages/components/src/Form/Inputs/InputSearch/InputSearchControls.tsx
+++ packages/components/src/Form/Inputs/InputSearch/InputSearchControls.tsx
@@ -63,7 +63,13 @@ export const InputSearchControls: FC<InputSearchControlsProps> = ({
   )
 
   return (
-    <Box mx="xxsmall" height="100%" display="flex" alignItems="center">
+    <Box
+      mx="xxsmall"
+      height="100%"
+      display="flex"
+      alignItems="center"
+      ml="auto"
+    >
       {summary && (
         <Text
           pr="xsmall"
