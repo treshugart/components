501dfd7c2b3ffcb61e64dd3df7be18ae953b604a
Daniel Christopher <dbchristopher@gmail.com>
2020-01-13 12:18:24 -0800

    [LENS-502] Confirm close dialog pattern

--- packages/playground/src/index.tsx
+++ packages/playground/src/index.tsx
@@ -24,13 +24,13 @@ import { GlobalStyle } from '@looker/components'
 import { theme } from '@looker/design-tokens'
 import { ThemeProvider } from 'styled-components'
 
-import { SelectDemo } from './Select/SelectDemo'
+import { CanCloseDialogManagerDemo } from './Confirm/CanCloseDialogManagerDemo'
 const App: React.FC = () => {
   return (
     <ThemeProvider theme={theme}>
       <>
         <GlobalStyle />
-        <SelectDemo />
+        <CanCloseDialogManagerDemo />
       </>
     </ThemeProvider>
   )
